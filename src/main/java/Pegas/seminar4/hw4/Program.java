badc11ff72b2e25e7c6c97eb120d2adb41db76dc
Dioneer <gulkina_em@mail.ru>
2024-04-15 18:16:32 +0300

    hql with param

--- src/main/java/Pegas/seminar4/hw4/Program.java
+++ src/main/java/Pegas/seminar4/hw4/Program.java
@@ -9,50 +9,56 @@ import java.time.LocalTime;
 
 public class Program {
     public static void main(String[] args) {
-        /**
-         * create
-         */
-        try(SessionFactory sessionFactory = HibernateConnectionUtil.buildSessionFactory();
-            Session session = sessionFactory.openSession()){
-            session.beginTransaction();
-            Course course = Course.builder()
-                    .title("Chemistry")
-                    .duration(LocalTime.parse("03:15:00"))
-                    .build();
-            session.persist(course);
-            session.close();
-            session.getTransaction().commit();
-        }
-        /**
-         * update
-         */
-        try(SessionFactory sessionFactory = HibernateConnectionUtil.buildSessionFactory();
-            Session session = sessionFactory.openSession()){
-            session.beginTransaction();
-            Course course = session.get(Course.class, 7);
-            course.setTitle("Java");
-//            session.clear();
-            session.getTransaction().commit();
-        }
+//        /**
+//         * create
+//         */
+//        try(SessionFactory sessionFactory = HibernateConnectionUtil.buildSessionFactory();
+//            Session session = sessionFactory.openSession()){
+//            session.beginTransaction();
+//            Course course = Course.builder()
+//                    .title("Chemistry")
+//                    .duration(LocalTime.parse("03:15:00"))
+//                    .build();
+//            session.persist(course);
+//            session.close();
+//            session.getTransaction().commit();
+//        }
+//        /**
+//         * update
+//         */
+//        try(SessionFactory sessionFactory = HibernateConnectionUtil.buildSessionFactory();
+//            Session session = sessionFactory.openSession()){
+//            session.beginTransaction();
+//            Course course = session.get(Course.class, 7);
+//            course.setTitle("Java");
+////            session.clear();
+//            session.getTransaction().commit();
+//        }
         /**
          * read
          */
         try(SessionFactory sessionFactory = HibernateConnectionUtil.buildSessionFactory();
             Session session = sessionFactory.openSession()){
             session.beginTransaction();
-            var course = session.createQuery("select c from Course c where c.id=11").list();
+            String title = "Java";
+            var course = session.createQuery("""
+                            select c from Course c
+                             where c.title=:title
+                            """)
+                    .setParameter("title", title)
+                    .list();
             System.out.println(course);
             session.getTransaction().commit();
         }
-        /**
-         * delete
-         */
-        try(SessionFactory sessionFactory = HibernateConnectionUtil.buildSessionFactory();
-            Session session = sessionFactory.openSession()){
-            session.beginTransaction();
-            Course course = session.get(Course.class, 4);
-            session.remove(course);
-            session.getTransaction().commit();
-        }
+//        /**
+//         * delete
+//         */
+//        try(SessionFactory sessionFactory = HibernateConnectionUtil.buildSessionFactory();
+//            Session session = sessionFactory.openSession()){
+//            session.beginTransaction();
+//            Course course = session.get(Course.class, 4);
+//            session.remove(course);
+//            session.getTransaction().commit();
+//        }
     }
 }
